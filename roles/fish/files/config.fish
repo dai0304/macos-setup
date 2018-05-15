a19b903ce9f05d1986b7bd394eb928d9b88bb13b
Daisuke Miyamoto <dai.0304@gmail.com>
2018-05-15 10:47:39 +0900

    fish plugins

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -1,8 +1,3 @@
-function fish_user_key_bindings
-  bind \cr 'peco_select_history (commandline -b)'
-  bind \c] peco_select_ghq_repository  # 追加
-end
-
 set -g theme_nerd_fonts yes
 
 ###############################################################################
